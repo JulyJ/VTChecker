67a57bf3ddc054e9b657d8737c8a840d0087f3fb
Julia <julia.koveshnikova@gmail.com>
2015-04-13 01:39:49 +0300

    Version 2.0
    # GUI added
    # File Explorer added
    # ReadMe added

---
+++ ReadMe.txt
@@ -0,0 +1,14 @@
+This is VirusTotal Checker version 2.0
+============================
+Simple AV detection checker for local file (using VirusTotal) — Edit
+============================
+To successfully run script you need to:
+- install Python 3.4+ 
+- have internet connection
+============================
+Usage:
+- Run script
+- Choose file to check
+- View report
+============================
+Contact: Julia Koveshnikova julia.koveshnikova@gmail.com
--- main.py
+++ main.py
@@ -1,32 +1,49 @@
 import os
 import sys
 import json
+from tkinter import *
+from tkinter import filedialog
+
 
 import hashgen
 import FileScan
 
+def fileProcessing(filePath):
+    md5 = hashgen.CheckSum.md5Checksum(filePath)
+    jsonPath = filePath + '_report.json'
+    f = open(jsonPath, 'wb')
+    f.write (FileScan.Report.retriveReport(md5))
+    f.close()
+    jsonFile = open(jsonPath, 'r')
+    jsonDict =  (json.loads(jsonFile.read()))
+    jsonFile.close()
+    return jsonDict
 
-for arg in sys.argv:
-    filePath = arg
+def parser(jsonDict):
+    result = ['Report:']
+    if jsonDict['response_code'] == 0:
+        result.append('No file info at VT')
+    else:
+        for av in jsonDict['scans']:
+            if jsonDict['scans'][av]['detected'] == True:
+               result.append('  ' + av + ' : ' + jsonDict['scans'][av]['result'])
+    return result
 
-md5 = hashgen.CheckSum.md5Checksum(filePath)
-# sha1 = hashgen.CheckSum.sha1Checksum(filePath)
-# sha256 = hashgen.CheckSum.sha256Checksum(filePath)
+def window_deleted():
+    root.quit()
 
-jsonPath = filePath + '_report.json'
-f = open(jsonPath, 'wb')
-f.write (FileScan.Report.retriveReport(md5))
-f.close()
+root = Tk()
+root.title('VT Checker')
+# root.geometry('500x400+300+200')
+root.protocol('WM_DELETE_WINDOW', window_deleted)
+root.resizable(True, False)
+filePath=filedialog.askopenfilename(parent=root)
+outputText = '\n'.join(parser(fileProcessing(filePath)))
+label1=Label(root,text=outputText,font='arial 10',justify='left',compound='left')
+label1.pack(side = 'top')
+root.mainloop()
 
-jsonFile = open(jsonPath, 'r')
-jsonDict =  (json.loads(jsonFile.read()))
-jsonFile.close()
 
 
-print ('\nResult:')
-if jsonDict['response_code'] == 0:
-    print ('No file info at VT')
-else:
-	for av in jsonDict['scans']:
-		if jsonDict['scans'][av]['detected'] == True:
-			print ('  ' + av + ' detects as ' + jsonDict['scans'][av]['result'])
+
+
